7a73612401d8c0cab72d44950975e2f847b157c9
JIBIN_BENNY <146290857+jibin006@users.noreply.github.com>
2024-09-21 16:55:37 +0530

    Update Netmiko_from file.py

--- Netmiko_from file.py
+++ Netmiko_from file.py
@@ -8,7 +8,7 @@ device_details = {
  }
 
 Connection_object = ConnectHandler(**device_details)
-config_path = r"C:\Users\jilub\OneDrive\Desktop\Python files\text.txt"
+config_path = r"C:\Users\<path>\OneDrive\Desktop\Python files\text.txt"
 Commands = Connection_object.send_config_from_file(config_file=config_path)
 print(Commands)
 Connection_object.disconnect()   
